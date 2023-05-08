bfd12c7627343ef16bcdbb0eaa8d3b313e098546
devaryakumar <dev.jha.arya@gmail.com>
2023-05-08 23:13:40 +0530

    WIP

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,7 +1,6 @@
-import { devConfig } from './dev.config'
 import { productionConfig } from './production.config'
 
 export const getConfig = () => {
   if (process.env.NODE_ENV === 'production') return productionConfig
-  return devConfig
+  return productionConfig
 }
